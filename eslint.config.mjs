d5b6b24946e6ac7af8c98c8c258b0f040e886cb7
domsen123 <dmarx@marxulm.de>
2025-07-29 10:10:26 +0200

    initial

---
+++ eslint.config.mjs
@@ -0,0 +1,26 @@
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  vue: true,
+  typescript: true,
+  rules: {
+    'antfu/top-level-function': 'off',
+    'no-console': 'off',
+    'vue/multi-word-component-names': 0,
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 0,
+    'vue/html-self-closing': [
+      'error',
+      {
+        html: {
+          void: 'any',
+          normal: 'any',
+          component: 'any',
+        },
+        svg: 'always',
+        math: 'always',
+      },
+    ],
+    'ts/no-empty-object-types': 'off',
+  },
+})
