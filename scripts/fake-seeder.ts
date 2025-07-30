f5503a8ecc8befe82bad2ce90233f07af66c57bb
Dominic Marx <dmarx@marxulm.de>
2025-07-30 10:28:14 +0200

    fix: fake seeder

--- scripts/fake-seeder.ts
+++ scripts/fake-seeder.ts
@@ -4,19 +4,25 @@ import { faker } from '@faker-js/faker'
 import { auth } from '../server/utils/auth'
 
 const seed = async () => {
-  for (let i = 0; i < 100; i++) {
-    await auth.api.signUpEmail({
-      body: {
-        name: faker.person.fullName(),
-        email: faker.internet.email(),
-        password: faker.internet.password({
-          length: 8,
-        }),
-      },
-    })
+  try {
+    for (let i = 0; i < 100; i++) {
+      await auth.api.signUpEmail({
+        body: {
+          name: faker.person.fullName(),
+          email: faker.internet.email(),
+          password: faker.internet.password({
+            length: 8,
+          }),
+        },
+      })
+    }
+    console.info('100 fake users created successfully.')
+    process.exit(0)
   }
-  console.info('100 fake users created successfully.')
-  process.exit(0)
+  catch (error) {
+    console.error('Error creating fake users:', error)
+    process.exit(1)
+  }
 }
 
 seed()
